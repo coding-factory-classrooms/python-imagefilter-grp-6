7c8e5ffebef75a3156c1787ff9139acfaf1eb8c9
abj95630 <abah20000@gmail.com>
2020-12-19 17:23:46 +0100

    Changement de variable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import cv2
 
 image = cv2.imread("data/imgs/automne.jpg")
-image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+image_gris = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-cv2.imwrite("data/output/automne.jpg", image_gray)
+cv2.imwrite("data/output/automne.jpg", image_gris)
